70444b33a8f097909e7a107cdbda27ece1d25ddd
Santosh Kushwaha <santoshskt9@gmail.com>
2023-01-21 14:58:26 +0530

    CHanges done in shared future page

--- src/pages/Topics/SharedFuture.jsx
+++ src/pages/Topics/SharedFuture.jsx
@@ -49,6 +49,12 @@ const SharedFuture = () => {
         {/* -------------------------- */}
         <h4 className="mt-4 fs-3">Youth in Legislature and Politics</h4>
         <p>
+          According to the United Nations Development Programme (UNDP) report, 2 out of 3 countries do not
+          consult young people as a part of the process of preparing poverty reduction strategies or national
+          development plans. Not only this but they are mostly underrepresented by formal political
+          institutions even though the political decisions impact their lives
+        </p>
+        <p>
           Meaningful democracies require meaningful participation from the young
           factions of society. The youth with their fresh ideas and objective
           opinions have a lot to offer- from creative thinking to revolutionary
@@ -56,10 +62,83 @@ const SharedFuture = () => {
           social responsibility, and enhance political institutions functioning.
         </p>
         <p>
+          Young men and women of today are key drivers for inclusive and progressive growth and sustainable
+          development. When these people are actively engaged and empowered in legislative
+          decision-making, societies shall be more cohesive, and resilient and the democracies will become
+          more vibrant.
+
+        </p>
+        <p>
           Besides, today’s young minds are tomorrow’s leaders and
           decision-makers, so their participation and engagement in legislature
           and politics are all the more crucial in the long run.
         </p>
+        <p>
+          The guiding principle for effective youth political participation must be, ‘nothing about us without
+          us.’ So, all strategies to enhance their role in politics must be grounded in a rights-based approach.
+        </p>
+        <p>
+          <i><b>Take at the following similar principles that occur when having good engagement from youth to
+            foster effective youth participation in politics:
+          </b></i>
+        </p>
+        <ul className="row row-cols-1  row-cols-md-2">
+          <li>
+            <h3>Transparency</h3>
+            <p>
+              This means having a crystal clear flow of the process of the purpose of youth
+              participation and engagement whether it is youth-led or organisation-led collaboration.
+            </p>
+          </li>
+          <li>
+            <h3>Innovation and Flexibility</h3>
+            <p>
+              This means having solid commitments from young people and
+              adults side to be open to new ideas and willing to undertake risks and challenge the existing
+              political processes and structures
+            </p>
+          </li>
+          <li>
+            <h3>Accountability</h3>
+            <p>
+              This means having proper mechanisms in place to ensure follow-ups,
+              implementation of youth ideas and accountability to constituencies. It majorly requires
+              young people to take an active role in monitoring and establishing channels where youth
+              participation in democratic events has proper visibility.
+            </p>
+          </li>
+          <li>
+            <h3>Sustainability</h3>
+            <p>
+              Sustainability of financial resources for best-practice youth engagement
+              initiatives can help to ensure these are not limited or one-off events or processes. Successful
+              youth engagement leads to decision-makers seeking youth involvement and leadership in
+              addressing challenges and designing solutions.
+
+            </p>
+          </li>
+          <li>
+            <h3>Capacity oriented </h3>
+            <p>
+              This means solidifying youth faction by supporting their capability
+              developments via networks, movements, youth organisations to enhance mutual
+              transparency, trust and contribution.
+            </p>
+          </li>
+          <li>
+            <h3>Relevant and Purposeful</h3>
+            <p>
+              Activities to enhance youth political participation should be as
+              youth-driven as possible. Young people themselves can decide on their priorities, methods,
+              and tactics. It means young people take on valued roles, addressing issues that are relevant
+              to them, and influencing real outcomes. To be relevant, they can link to specific concerns of
+              youth such as unemployment, climate change, digital transformation, pandemic work
+              culture, etc.
+
+            </p>
+          </li>
+        </ul>
+
         <p>
           India is known to be the world’s largest democratic nation with more
           than half of the population between the age groups of 18-35 years.
@@ -139,7 +218,18 @@ const SharedFuture = () => {
           towards politicians and party ideologies.
         </p>
         <p>
-          Moreover, they still feel directionless and often marginalised from
+          According to a youth stocktaking report by the Organisation of Economic Co-operation and
+          Development (OECD), in 17 out of 35 OECD member countries, young people express lesser trust in
+          governments than in their parents. It signals from the frustration with the present outdated channels
+          to make their voices heard in the political sphere.
+
+        </p>
+        <p>
+          Also, the significant share of political disengagement is vital as around 25% of 15-29 years old stress
+          that they are not ‘ interested in politics.’
+        </p>
+        <p>
+          Youth feel directionless and often marginalised from
           mainstream activities, which is a threat to democracy. The best
           solution can be to offer more youth involvement in international
           campaigns and challenging activities that make them feel heard and
@@ -174,8 +264,49 @@ const SharedFuture = () => {
           developmental process.
         </p>
         <p>
-          Youth participation and representation globally in policy-making
-          institutions have always been relatively low. People under the age of
+          Basically, every nation needs to empower its youth so that they may contribute to society and the
+          economy. This undertaking needs a clear and comprehensive vision that is backed by a sound
+          strategy, and a specific action plan that guides youth-related policies, services and programmes.
+        </p>
+        <p>
+          This can be done by formulating a youth policy. A foremost public policy that acts as a framework o
+          shape the vision for youth as well as builds a repertoire for good governance for the lawmakers.
+          There are various examples of national strategies and policies that focus on young people’s
+          participation in policy and governance like
+        </p>
+        <ul className="fs-5 py-3">
+          <li className="py-2">
+            1. <b>Slovak Republic’s National Youth Strategy</b> (2014-2020) provides a cross-sectorial approach
+            and focuses on nine policy areas including education, employment, health,
+            entrepreneurship, social inclusion and volunteering. The strategy underlines investment and
+            empowerment as its key pillars while placing an emphasis on the labour migration of
+            educated young Slovaks to other countries
+          </li>
+          <li className="py-2">
+            2. <b>In Ireland, a national strategy on LGBTI+ youth</b> (2018-2020) has been developed to create a
+            more supportive and inclusive environment for LGBTI+ young people.
+          </li>
+          <li className="py-2">
+            3. <b>In Gaia, Portugal the municipal corporation has prepared a municipal youth plan</b> to enable
+            public policies fostering equal opportunities and social cohesion; and to implement local
+            youth policies in an open collaborative manner.
+
+          </li>
+        </ul>
+        <p>
+          However, it also needs to be understood well that just formulating such strategies by the government
+          isn’t enough. They need to ensure that these policies are evidence-based, transparent, accountable
+          and properly executed. Adequate resources as well as sound monitoring and evaluation mechanisms
+          are critical to translating youth commitments into tangible outcomes.
+        </p>
+        <p>
+          Currently, COVID-19 has increased already high levels of uncertainty for youth due to global
+          transformations that have significantly affected their access to decent jobs, affordable education and
+          housing, and opportunities to participate in public and political life. Thus, youth participation and representation globally in policy-making
+          institutions have always been relatively low.
+        </p>
+        <p>
+          People under the age of
           35 are seldom seen taking active roles in public administration,
           parliamentary sessions or any governing committees on economic growth
           and development. This issue needs to be addressed more sequentially
@@ -219,6 +350,40 @@ const SharedFuture = () => {
               the Indian government’s Digital India campaign portals like
               ‘mygov.in’ connects their government and citizens under the
               universal brotherhood
+            </span>
+          </li>
+          <li className="list-group-item border-0  ps-0">
+            <span className="p-2 bg-primary clip-cicle text-white mt-2">
+              <i className="bi bi-arrow-right"></i>
+            </span>{" "}
+            <span className="ps-2 fs-5">
+              <b>Developing budgeting as a tool can be powerful</b> to align the economic and social objectives
+              of governments with the needs of young people by integrating their perspective within the
+              budgetary process. For instance, Canada anticipates the impact of budgetary allocations
+              across different age cohorts in the framework of Gender Budgeting to assess the impact on
+              younger and older age cohorts.
+            </span>
+          </li>
+          <li className="list-group-item border-0  ps-0">
+            <span className="p-2 bg-primary clip-cicle text-white mt-2">
+              <i className="bi bi-arrow-right"></i>
+            </span>{" "}
+            <span className="ps-2 fs-5">
+              <b>Strengthening youth’s relationship with public institutions is vital</b> and for that government
+              can enrol youth stakeholders throughout the policy cycle to ensure age diversity and more
+              responsive and inclusive policy outcomes, through in-person and digital means.
+            </span>
+          </li>
+          <li className="list-group-item border-0  ps-0">
+            <span className="p-2 bg-primary clip-cicle text-white mt-2">
+              <i className="bi bi-arrow-right"></i>
+            </span>{" "}
+            <span className="ps-2 fs-5">
+              The pandemic has strongly indicated that young volunteers and youth workers are a crucial
+              resource in <b>ensuring societal resilience in the face of shocks and disasters.</b> For that,
+              governments and institutions can solidify volunteering and youth work through national
+              laws, strategies and programmes that include a common vision and, co-ordinated action for
+              building youth’s skills and competencies as well as social cohesion and societal resilience.
             </span>
           </li>
         </ul>
